a41368fe78f846ce1d8dd9bf426ce86780fe345d
Audrey Bertin <abertin27@massmutual.com>
2021-08-16 10:50:34 -0400

    replace NAs

--- Data-Cleaning/clean_school_info.R
+++ Data-Cleaning/clean_school_info.R
@@ -2,5 +2,48 @@
 # Cleaning the data files from the Data/School_Details folder
 # Packages required: readr, tidyverse
 
+library(tidyverse)
+
+
+# Read in the datasets
 
 Enrollment <- readr::read_csv("Data/School_Details/Enrollment.csv")
+Expenditures <- readr::read_csv("Data/School_Details/Expenditures.csv")
+Support <- readr::read_csv("Data/School_Details/Support.csv")
+
+# Replace negative values with NA
+
+Enrollment <- Enrollment %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -9))) %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -8))) %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -7))) %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -6))) %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -5))) %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -4))) %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -3))) %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -2))) %>%
+  mutate(across(SCH_PSENR_NONIDEA_A3:SCH_ENR_504_M, ~na_if(., -1)))
+
+Expenditures <- Expenditures %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -9))) %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -8))) %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -7))) %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -6))) %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -5))) %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -4))) %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -3))) %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -2))) %>%
+  mutate(across(SCH_SAL_TOTPERS_WOFED:SCH_NPE_WFED, ~na_if(., -1)))
+
+Support <- Support %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -9))) %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -8))) %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -7))) %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -6))) %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -5))) %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -4))) %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -3))) %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -2))) %>%
+  mutate(across(SCH_FTETEACH_TOT:SCH_FTESERVICES_SOC, ~na_if(., -1)))
+
+
